98fb71dd17f758e682f8e032a6f09cb1f4760dec
afshar96 <afshar96@gmail.com>
2025-10-22 08:25:44 +0330

    add socket package and test server socket

---
+++ lib/massage_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:socket_io_client/socket_io_client.dart';
+
+class MassageScreen extends StatefulWidget {
+  const MassageScreen({super.key});
+
+  @override
+  State<MassageScreen> createState() => _MassageScreenState();
+}
+
+class _MassageScreenState extends State<MassageScreen> {
+  late Socket socket;
+  final ctrl = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    initSocket();
+  }
+
+  initSocket() {
+    socket = io(
+      'http://192.168.5.128:3003',
+      OptionBuilder()
+          .setTransports(["websocket"])
+          .disableAutoConnect()
+          .enableForceNew()
+          .build(),
+    );
+    socket.onConnect((data) => print('onConnect:$data'));
+    socket.onDisconnect((data) => print('onDisconnect:$data'));
+    socket.onError((data) => print('onError:$data'));
+    socket.connect();
+    socket.on('welcome', (data) => print('WELCOME:$data'));
+    socket.on('massage', (data) => print(' MASSAGE:$data'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(controller: ctrl),
+            SizedBox(height: 30.0),
+            TextButton(
+              onPressed: () {
+                socket.emit('message', ctrl.text);
+                ctrl.clear();
+              },
+              child: Text('Send Message'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
